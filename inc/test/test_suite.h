afb3e8b232ac8fb348f006a822c1723efd536f4c
Joel Dewey <joel.s.dewey@ieee.org>
2015-02-24 20:49:12 -0500

    Created static functions for testing. Removed GPS tests. Added delay function and reordered defines.

--- inc/test/test_suite.h
+++ inc/test/test_suite.h
@@ -8,8 +8,21 @@
  */
 extern void ExecuteTests(long tests, unsigned char delay);
 
+/**
+ * @struct UnitTest
+ * A struct for running tests. PreDelay performs a delay before the test, test
+ * indicates what test will be performed, and PostDelay performs a delay after the test.
+ */
+typedef struct {
+	int PreDelay;
+	int Test;
+	int PostDelay;
+} UnitTest;
+
 #ifndef TEST_SUITE_H_
 #define TEST_SUITE_H_
+
+#define CYCLE_TIME 63 // Rounded up from 62.5.
 
 /**
  * @defgroup testDefines Test Defines
@@ -17,15 +30,12 @@ extern void ExecuteTests(long tests, unsigned char delay);
  */
 #define BLINKY 			0x01 	   // LED blinking test.
 #define RS232_PC 		0x02 	   // Read and write to the PC via RS-232.
-#define RS232_GPS 		0x04 	   // Read and write to the GPS module via RS-232.
-#define ADC				0x08 	   // Read from the ADC and print the results out to the console.
-#define CAN_MPPT_ON		0x10 	   // Enable MPPTs using CAN.
-#define CAN_MPPT_OFF	0x20 	   // Disable MPPTs using CAN.
-#define CAN_MPPT_DATA	0x40 	   // Retrieve data from the MPPTs via CAN.
-#define CAN_CAR_READ	0x80 	   // Read from the car's CAN.
-#define CAN_CAR_WRITE	0x100 	   // Send data frame from AC to CAN bus.
-
-#define DELAY			0x80000000 // Enable delay between each test. This is a parameter
+#define ADC				0x03 	   // Read from the ADC and print the results out to the console.
+#define CAN_MPPT_ON		0x04 	   // Enable MPPTs using CAN.
+#define CAN_MPPT_OFF	0x05 	   // Disable MPPTs using CAN.
+#define CAN_MPPT_DATA	0x06 	   // Retrieve data from the MPPTs via CAN.
+#define CAN_CAR_READ	0x07 	   // Read from the car's CAN.
+#define CAN_CAR_WRITE	0x08 	   // Send data frame from AC to CAN bus.
 /**
  * @}
  */
--- test/test_suite.c
+++ test/test_suite.c
@@ -10,12 +10,81 @@
 #include "../inc/test/test_suite.h"
 
 /**
- * The main function for executing tests for the Array Controller.
- *
- * @param[in] tests A long which each individual bit is assigned to a test.
- * @param[in] delay A delay (in milliseconds) to execute between tests.
+ * A simple test blinking the LED multiple times.
  */
-extern void ExecuteTests(long tests, unsigned char delay) {
-
+static void Blinky() {
+	/// @todo Implement.
 }
 
+/**
+ * Read and write to the PC using the RS-232 controller on the
+ * Array Controller.
+ */
+static void RS232_PC() {
+	/// @todo Implement.
+}
+
+/**
+ * Read values from the ADC and print them to the console.
+ */
+static void ADC() {
+	/// @todo Implement.
+}
+
+/**
+ * Enable the MPPTs through CAN.
+ */
+static void CAN_MPPT_Enable() {
+	/// @todo Implement.
+}
+
+/**
+ * Disable the MPPTs through CAN.
+ */
+static void CAN_MPPT_Disable() {
+	/// @todo Implement.
+}
+
+/**
+ * Retrieve data from the MPPTs using CAN.
+ */
+static void CAN_MPPT_Data() {
+	/// @todo Implement.
+}
+
+/**
+ * Read data from the solar car's main CAN bus.
+ */
+static void CAN_Car_Read() {
+	/// @todo Implement.
+}
+
+/**
+ * Write data to the solar car's main CAN bus.
+ */
+static void CAN_Car_Write() {
+	/// @todo Implement.
+}
+
+/**
+ * A wrapper for __delay_cycles() which allows the developer to create a delay
+ * of a number in microseconds.
+ *
+ * @note This should not be used in conjunction with functions that rely on ISRs!
+ * @note This function will support a max delay of 65535 microseconds.
+ *
+ * @param[in] time The duration of the delay in microseconds.
+ */
+static void Delay(int time) {
+	__delay_cycles((time * 1000)/CYCLE_TIME);
+}
+
+/**
+ * The main function for executing tests for the Array Controller.
+ *
+ * @param[in] tests Describes each test via the use of the UnitTest structure.
+ * @param[in] numOfTests The number of tests to be run.
+ */
+extern void ExecuteTests(UnitTest tests[], int numOfTests) {
+
+}
