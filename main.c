5fa3e426d1cb256243fe8f7f0b1a220799db9585
Joel Dewey <joel.s.dewey@ieee.org>
2015-02-25 10:48:54 -0500

    Removed GPS interrupt as GPS is no longer a part of the project.

--- main.c
+++ main.c
@@ -20,54 +20,7 @@ int main(void) {
 /*
  * Interrupts
  */
-
 /*
-* BPS2GPS Interrupt Service Routine
-*/
-	#pragma vector=USCI_A0_VECTOR
-__interrupt void USCI_A0_ISR(void)
-{
-	extern char *put_ptr, *get_ptr;
-	extern char put_status, get_status, BPS2GPS_RX_flag;
-	char ch;
-
-    switch(__even_in_range(UCA0IV,16))
-    {
-    case 0:			                          // Vector 0 - no interrupt
-      break;
-    case 2:                                   // Data Received - UCRXIFG
-          ch = UCA0RXBUF;
-          *get_ptr++ = ch;
-          if (ch == 0x0D){
-             *get_ptr = 0;
-             get_ptr = &test_buffer[0];
-             BPS2GPS_RX_flag = TRUE;
-             RX_INT_count++;
-		     get_status = FALSE;
-          }
-          else
-          {
-		     get_status = TRUE;
-         }
-     break;
-    case 4:                                   // TX Buffer Empty - UCTXIFG
-      ch = *put_ptr++;
-	  if (ch == '\0')
-	  {
-	  	UCA0IE &= ~UCTXIE;
-		put_status = FALSE;
-	  }
-	  else
-	  {
-		UCA0TXBUF = ch;
-	  }
-      break;
-    default:
-      break;
-    }
-}
-
-	/*
 * BPS2PC Interrupt Service Routine
 */
 	#pragma vector=USCI_A3_VECTOR
