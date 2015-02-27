6b7da59700b9dd829b1c716ac0242c571150a260
Joel Dewey <joel.s.dewey@ieee.org>
2015-02-27 15:15:55 -0500

    Fixing git errors.

--- test/test_suite.c
+++ test/test_suite.c
@@ -41,12 +41,16 @@ static void Delay(unsigned int delayConstant) {
  */
 static void Blinky() {
 	unsigned int i;
+	unsigned int compareVal = 65535;
 
 	P1DIR |= BIT0;
+	P2DIR |= BIT0;
+	P4DIR |= BIT0 | BIT1 | BIT2 | BIT3;
 
-	for (i = 0; i < 65535; i++) {
+	for (i = 0; i < compareVal; i++) {
 		P1OUT ^= BIT0;
-		Delay(DELAY_100);
+		P2OUT ^= BIT0;
+		P4OUT ^= BIT0 | BIT1 | BIT2 | BIT3
 	}
 }
 
