165dfcf70e547f350357e0595aa175a3785236d2
Joel Dewey <joel.s.dewey@ieee.org>
2015-02-28 14:36:21 -0500

    Commented out interrupt for now.

--- main.c
+++ main.c
@@ -50,44 +50,44 @@ extern void Delay(unsigned int delayConstant) {
 /*
 * BPS2PC Interrupt Service Routine
 */
-	#pragma vector=USCI_A3_VECTOR
-__interrupt void USCI_A3_ISR(void)
-{
-	extern char *putPC_ptr, *getPC_ptr;
-	extern char put_status_PC, get_status_PC, BPS2PC_RX_flag;
-	char ch;
-
-    switch(__even_in_range(UCA3IV,16))
-    {
-    case 0:		                              // Vector 0 - no interrupt
-      break;
-    case 2:                                   // Data Received - UCRXIFG
-          ch = UCA3RXBUF;
-          *getPC_ptr++ = ch;
-          if (ch == 0x0D){
-             *getPC_ptr = 0;
-             getPC_ptr = &test_buffer_PC[0];
-             BPS2PC_RX_flag = TRUE;
-		     get_status_PC = FALSE;
-          }
-          else
-          {
-		     get_status_PC = TRUE;
-         }
-     break;
-    case 4:                                   // TX Buffer Empty - UCTXIFG
-      ch = *putPC_ptr++;
-	  if (ch == '\0')
-	  {
-	  	UCA3IE &= ~UCTXIE;
-		put_status_PC = FALSE;
-	  }
-	  else
-	  {
-		UCA3TXBUF = ch;
-	  }
-      break;
-    default:
-      break;
-    }
-}
+//	#pragma vector=USCI_A3_VECTOR
+//__interrupt void USCI_A3_ISR(void)
+//{
+//	extern char *putPC_ptr, *getPC_ptr;
+//	extern char put_status_PC, get_status_PC, BPS2PC_RX_flag;
+//	char ch;
+//
+//    switch(__even_in_range(UCA3IV,16))
+//    {
+//    case 0:		                              // Vector 0 - no interrupt
+//      break;
+//    case 2:                                   // Data Received - UCRXIFG
+//          ch = UCA3RXBUF;
+//          *getPC_ptr++ = ch;
+//          if (ch == 0x0D){
+//             *getPC_ptr = 0;
+//             getPC_ptr = &test_buffer_PC[0];
+//             BPS2PC_RX_flag = TRUE;
+//		     get_status_PC = FALSE;
+//          }
+//          else
+//          {
+//		     get_status_PC = TRUE;
+//         }
+//     break;
+//    case 4:                                   // TX Buffer Empty - UCTXIFG
+//      ch = *putPC_ptr++;
+//	  if (ch == '\0')
+//	  {
+//	  	UCA3IE &= ~UCTXIE;
+//		put_status_PC = FALSE;
+//	  }
+//	  else
+//	  {
+//		UCA3TXBUF = ch;
+//	  }
+//      break;
+//    default:
+//      break;
+//    }
+//}
